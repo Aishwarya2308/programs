d0571f774f3a4941ec0c29149a2a1e80053ed0c6
Aishwarya2308 <39359434+Aishwarya2308@users.noreply.github.com>
2025-02-27 18:03:38 +0530

    Create productExceptSelf.java

---
+++ productExceptSelf.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int arr[]=new int[nums.length];
+        for(int i=0; i<nums.length; i++){
+            int pro=1;
+            for (int j=0; j<nums.length; j++){
+            if(i==j)continue;
+            pro*=nums[j];
+        }
+        arr[i]=pro;
+    }
+    return arr;
+    }
+}
